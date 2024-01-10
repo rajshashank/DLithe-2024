5f463c1e25a7712ec2e6558ea11669e9e3d726ff
Shashank Raj <115774146+rajshashank@users.noreply.github.com>
2024-01-11 00:47:39 +0530

    Create sqroot.java

---
+++ sqroot.java
@@ -0,0 +1,12 @@
+import java.math.*;
+import java.util.Scanner;
+
+public class sqroot {
+    public static void main(String args[]) {
+        System.out.println("Enter the num: ");
+        Scanner sin = new Scanner(System.in);
+        int p = sin.nextInt();
+        System.out.println((int)(Math.sqrt(p)));
+
+    }
+}
