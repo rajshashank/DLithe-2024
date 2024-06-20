2ee1bd403770213dc617847e7ebbbd86b56d541b
rajshashank <rajshashank917@gmail.com>
2024-06-20 20:54:55 +0530

    bs

--- 6th sem/pattern.java
+++ 6th sem/pattern.java
@@ -21,6 +21,9 @@ public class pattern {
         for (int i = 0; i < 10; i++) {
             for(int j = 0; j <= i; j++) {
                 System.out.println();
+
+asckjhascuh
+
             }
             
         }
